d70494ae4c90eab0d3551ba4148011b709e31a6c
Priyanka Babu <priyankaanswers@gmail.com>
2016-09-28 09:13:33 +0530

    Update

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-app = express();
-
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname })
-});
-
-var server = app.listen(process.env.PORT || 80);
